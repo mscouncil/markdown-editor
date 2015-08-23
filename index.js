816e65e50bae33042c0928e88189c488b2193827
pemrouz <p3pedram@hotmail.com>
2015-08-23 13:10:35 +0100

    export simple array of resources

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-var ripple = require('rijs')()  
-  , file   = require('utilise.file')
-  , fs     = require('fs')
+var file   = require('utilise.file')
 
-ripple
-  .resource(
-      'markdown-editor'
-    , require('./resources/markdown-editor')
-    )
-  .resource(
-      'markdown-preview'
-    , require('./resources/markdown-preview')
-    )
-  .resource(
-      'markdown-editor.css'
-    , file(__dirname + '/resources/markdown-editor.css')
-    )
-  .resource(
-      'markdown-preview.css'
-    , file(__dirname + '/resources/markdown-preview.css')
-    )
-
-module.exports = ripple
+module.exports = [
+  { 
+    name: 'markdown-editor'
+  , body: require('./resources/markdown-editor')
+  }
+, { 
+    name: 'markdown-preview'
+  , body: require('./resources/markdown-preview')
+  }
+, { 
+    name: 'markdown-editor.css'
+  , body: file(__dirname + '/resources/markdown-editor.css')
+  }
+, {
+    name: 'markdown-preview.css'
+  , body: file(__dirname + '/resources/markdown-preview.css')
+  }
+]
