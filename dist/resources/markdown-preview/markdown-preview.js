f511c60ceebea6b0d82799e2e449db6e866d797f
pemrouz <p3pedram@hotmail.com>
2016-07-31 17:52:20 +0100

    chore: build dist

--- dist/resources/markdown-preview/markdown-preview.js
+++ dist/resources/markdown-preview/markdown-preview.js
@@ -4,17 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = markdownPreview;
-
-var _remarkable = require('remarkable');
-
-var _remarkable2 = _interopRequireDefault(_remarkable);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function markdownPreview(_ref) {
   var value = _ref.value;
 
-  var mark = new _remarkable2.default({ breaks: true, linkify: true }),
+  var mark = new Remarkable({ breaks: true, linkify: true }),
       md = mark.render.bind(mark),
       decode = function decode(d) {
     return d.replace(/\\r\\n/mg, '\n');
